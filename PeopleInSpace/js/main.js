a7de1f8e07e296384d6bf751bae82db020cdc5dc
shreeram0k <44352134+shreeram0k@users.noreply.github.com>
2019-04-06 11:21:24 +0530

    first commit

---
+++ PeopleInSpace/js/main.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+	"use strict";
+
+	var window_width 	 = $(window).width(),
+	window_height 		 = window.innerHeight,
+	header_height 		 = $(".default-header").height(),
+	header_height_static = $(".site-header.static").outerHeight(),
+	fitscreen 			 = window_height - header_height;
+
+
+	$(".fullscreen").css("height", window_height)
+	$(".fitscreen").css("height", fitscreen);
+
+ });
